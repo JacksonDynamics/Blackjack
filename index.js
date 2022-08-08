f66b6c05cbc9d85ec187ebb2073373b2c921a1a9
studmanj <bjackso3@hotmail.com>
2022-08-07 18:54:46 -0600

    Random Variable sum

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+
+
+let sum = firstCard + secondCard;
+
+console.log(firstCard,secondCard)
+console.log(sum)
