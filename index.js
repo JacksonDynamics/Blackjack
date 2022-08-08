46d7228b55e697e5efaf2365326683e4864b8127
studmanj <bjackso3@hotmail.com>
2022-08-08 00:01:42 -0600

    If/Else Statements

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
 let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-
-
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-console.log(firstCard,secondCard)
-console.log(sum)
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    }
+    else if (sum === 21) {
+        hasBlackJack = true;
+        message = "Wahoo! You got a blackjack! 🥳";
+    }
+    else {
+        isAlive = false;
+        message = "You're out of the game! 😭";
+    }
+    
+    // Cash out
+    console.log(message)
+}
+
+
